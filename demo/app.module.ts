5dbfaf930d184cf5cadf79172998868626a329ba
twinssbc <bocong@hotmail.com>
2016-10-17 23:51:57 +0800

    checkin missing files;

---
+++ demo/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app/app.component';
+import { AboutPage } from './pages/about';
+import { HomePage } from './pages/home';
+import { TabsPage } from './pages/tabs';
+import { NgCalendarModule  } from 'ionic2-calendar';
+
+
+@NgModule({
+    declarations: [
+        MyApp,
+        AboutPage,
+        HomePage,
+        TabsPage
+    ],
+    imports: [
+        NgCalendarModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        AboutPage,
+        HomePage,
+        TabsPage
+    ]
+})
+export class AppModule {}
