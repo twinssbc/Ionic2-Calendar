f18fbaf8cb7f2e0316ad6ccae60d900e22592986
Heath Corbin <hcorbin@hyperfaze.com>
2017-02-27 10:28:18 -0600

    Made a tweak to the display logic for all day

--- src/calendar.ts
+++ src/calendar.ts
@@ -127,8 +127,8 @@ export enum Step {
                             -
                             {{event.endTime|date: 'HH:mm'}}
                         </span>
-                    <span *ngIf="event.allDay" class="monthview-eventdetail-timecolumn">{{allDayLabel || 'All day'}}</span>
-                    <span class="event-detail"> <span *ngIf="!allDayLabel || allDayLabel !== ''">|</span>  {{event.title}}</span>
+                    <span *ngIf="event.allDay" class="monthview-eventdetail-timecolumn">{{allDayLabel !== undefined ? allDayLabel : 'All day'}}</span>
+                    <span class="event-detail"> <span *ngIf="allDayLabel === undefined || allDayLabel.length">|</span>  {{event.title}}</span>
                 </ion-item>
                 <ion-item *ngIf="selectedDate?.events.length==0">
                     <div class="no-events-label">{{noEventsLabel}}</div>
